544672a8c3d2e53efca4e83d3df23c39eab1e745
gniadoo <gniadek.jakub1@gmail.com>
2021-08-23 11:25:58 +0200

    Add testing files

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -4,49 +4,53 @@ from .models import Question, Choice
 from django.template import loader
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 
-#Hard coded
+# Hard coded
 # def index(request):
 #     latest_question_list = Question.objects.order_by('-pub_date')[:5]
 #     context = {'latest_question_list': latest_question_list}
 #     return render(request, 'polls/index.html', context)
 
 
-#Generic view
+# Generic view
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 
-#Hard coded
+# Hard coded
 # def detail(request, question_id):
 #     question = get_object_or_404(Question, pk=question_id)
 #     return render(request, 'polls/detail.html', {'question': question})
 
 
-#Generic view
+# Generic view
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
 
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
-#Hard coded
+
+# Hard coded
 # def results(request, question_id):
 #     question = get_object_or_404(Question, pk=question_id)
 #     return render(request, 'polls/results.html', {'question': question})
 
 
-#Generic view
+# Generic view
 class ResultsView(generic.DetailView):
     model = Question
     template_name = 'polls/results.html'
 
 
-#Hard coded
+# Hard coded
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     try:
@@ -60,4 +64,3 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-
